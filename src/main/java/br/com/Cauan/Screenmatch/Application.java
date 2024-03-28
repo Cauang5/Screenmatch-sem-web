9dc3fa7ed0f9d5bbb3cd581bd8e5f5c01b9de82d
Cauan Leite <cauanfc@hotmail.com>
2024-03-28 19:39:13 -0300

    Initial commit

---
+++ src/main/java/br/com/Cauan/Screenmatch/Application.java
@@ -0,0 +1,27 @@
+package br.com.Cauan.Screenmatch;
+
+import br.com.Cauan.Screenmatch.model.DadosSerie;
+import br.com.Cauan.Screenmatch.service.ConsumoAPI;
+import br.com.Cauan.Screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=Vikings&apikey=9e2002a7");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
